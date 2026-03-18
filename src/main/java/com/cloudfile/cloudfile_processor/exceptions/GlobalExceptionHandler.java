c7f63300c17881ac19ac79f71c7fcbc5e55b8d7d
diegobrsantosdev <diegobrsantosdev@gmail.com>
2026-03-18 19:32:01 +0000

    added stacktracelog and refactoring ErrorResponse exception

--- src/main/java/com/cloudfile/cloudfile_processor/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cloudfile/cloudfile_processor/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.cloudfile.cloudfile_processor.exceptions;
 
 import com.cloudfile.cloudfile_processor.dto.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +14,8 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationErrors(
@@ -46,11 +50,16 @@ public class GlobalExceptionHandler extends RuntimeException {
         return ResponseEntity.status(500).body(response);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGeneric(Exception ex) {
 
-        Map<String, String> response = new HashMap<>();
-        response.put("error", "Internal server error");
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
+
+        log.error("Unexpected error occurred", ex);
+
+        ErrorResponse response = new ErrorResponse(
+                "Internal server error",
+                null
+        );
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
