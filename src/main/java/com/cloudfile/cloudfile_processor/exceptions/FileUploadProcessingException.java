6a9237f503654946899965f5209f81e90ce94717
diegobrsantosdev <diegobrsantosdev@gmail.com>
2026-03-18 17:06:07 +0000

    fileUploadProcessing and invalidFileUploadRequest exceptions

---
+++ src/main/java/com/cloudfile/cloudfile_processor/exceptions/FileUploadProcessingException.java
@@ -0,0 +1,17 @@
+package com.cloudfile.cloudfile_processor.exceptions;
+
+public class FileUploadProcessingException extends RuntimeException {
+
+    public FileUploadProcessingException(String message) {
+        super(message);
+    }
+
+    public FileUploadProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+
+
+
+
